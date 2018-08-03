3706f9c6783f529c1e7bad9af2a30acc3a25112a
leogtzr <leogutierrezramirez@gmail.com>
2018-08-03 11:11:32 -0600

    Showing books count

--- src/main/webapp/app/entities/book/book.tsx
+++ src/main/webapp/app/entities/book/book.tsx
@@ -30,7 +30,7 @@ export class Book extends React.Component<IBookProps> {
           </Link>
         </h2>
         <div className="table-responsive">
-          <h2>bookList.size</h2>
+          <h2>Current books: {bookList.size}</h2>
           <Table responsive>
             <thead>
               <tr>
