2bf17e204a72a6b98508e35fe562411b8ee721c1
Leo <leogutierrezramirez@gmail.com>
2018-09-21 23:09:07 -0600

    changing permissions

--- src/main/java/com/mybooks/config/SecurityConfiguration.java
+++ src/main/java/com/mybooks/config/SecurityConfiguration.java
@@ -107,7 +107,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/api/account/reset-password/init").permitAll()
             .antMatchers("/api/account/reset-password/finish").permitAll()
             .antMatchers("/api/all").permitAll()
-            .antMatchers("/api/books/**").authenticated()
+            .antMatchers("/api/books/**").permitAll()
             .antMatchers("/management/health").permitAll()
             .antMatchers("/management/info").permitAll()
             .antMatchers("/management/**").hasAuthority(AuthoritiesConstants.ADMIN)
